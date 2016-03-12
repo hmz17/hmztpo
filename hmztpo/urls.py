b259393572540eff46ba0e6785bdb3fac6225b85
Hamza Mirza <hamzamirza95@gmail.com>
2016-03-12 23:51:11 +0530

    Fourth

--- hmztpo/urls.py
+++ hmztpo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from tpo import views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^$', views.home),
+    url(r'^logout$', views.logout),
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
 ]
--- tpo/views.py
+++ tpo/views.py
@@ -1,3 +1,31 @@
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template import RequestContext
+
+from django.shortcuts import render_to_response
+import json
+
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout, login
+
+from social.backends.oauth import BaseOAuth1, BaseOAuth2
+from social.backends.google import GooglePlusAuth
+from social.backends.utils import load_backends
+
 
 # Create your views here.
+def home(request):
+   context = RequestContext(request,
+                           {'request': request,
+                            'user': request.user})
+   return render_to_response('thirdauth/home.html',
+                             context_instance=context)
+
+def logout(request):
+    """Logs out user"""
+    auth_logout(request)
+    return redirect('/')
